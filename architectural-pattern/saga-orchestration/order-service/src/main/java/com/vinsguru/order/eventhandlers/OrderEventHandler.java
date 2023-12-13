8804b658026401a2a87a34e870dbf962314f9b98
Chul Won Choe <cwchoe@hotmail.com>
2023-12-13 10:35:42 +0900

    라이브러리 최신화

--- architectural-pattern/saga-orchestration/order-service/src/main/java/com/vinsguru/order/eventhandlers/OrderEventHandler.java
+++ architectural-pattern/saga-orchestration/order-service/src/main/java/com/vinsguru/order/eventhandlers/OrderEventHandler.java
@@ -6,7 +6,6 @@ import com.vinsguru.order.service.OrderEventUpdateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.core.publisher.DirectProcessor;
 import reactor.core.publisher.Flux;
 
 import java.util.function.Consumer;
